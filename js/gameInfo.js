47122e29026feadf49edc14e57a3566089776e71
JeoungB <eoehdektnv00@gmail.com>
2025-04-26 23:53:52 +0900

    '26'

--- js/gameInfo.js
+++ js/gameInfo.js
@@ -1,12 +1,14 @@
 // 게임 이동 버튼
-
 const gameList = document.querySelectorAll('.game-list li');
 
 gameList.forEach(gameMoveBtn => {
     gameMoveBtn.addEventListener('click', e => {
+        let cardLink = `/cardGame.html`;
         let btn = e.target;
         if(btn.className === 'card-game') {
-            console.log('d')
-        }
+            location.href = cardLink;
+        };
     });
 });
+
+// 뽑기 버튼
